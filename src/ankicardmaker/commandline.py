bb97656e24aca13856a8883320fdd42af856dfd8
Attila Zsolt Somogyi <attila.zsolt.somogyi@gmail.com>
2024-01-04 21:40:29 +0100

    fix: fix the module load

--- src/ankicardmaker/commandline.py
+++ src/ankicardmaker/commandline.py
@@ -1,6 +1,6 @@
 """Module providing a function to handle command line arguments."""
 
-import argparse
+from argparse import ArgumentParser
 from ankicardmaker.languages import Language
 
 
@@ -8,7 +8,7 @@ class CommandLine:
     """Class providing a function to handle command line arguments."""
 
     def __init__(self):
-        self.parser = argparse.ArgumentParser(description="Anki Card Maker")
+        self.parser = ArgumentParser(description="Anki Card Maker")
         self.parser.add_argument("--version", action="version", version="%(prog)s 0.1")
         self.parser.add_argument(
             "-d", "--deck", dest="deck", help="Deck name", required=True, nargs=1
